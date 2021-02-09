9f2cacd16b3b391fff12dac4720950714241111b
Md,Foriduzzaman Zihad <78448945+foriduzzaaman-zihad@users.noreply.github.com>
2021-02-10 02:55:01 +0600

    Add files via upload

---
+++ 1118.py
@@ -0,0 +1,29 @@
+n = 12
+m = []
+st1 = 1
+st2 = n - 2
+s = 0
+v = 0
+i3 = n - 1
+
+t = raw_input()
+
+for i in range(n):
+    l = []
+    for i2 in range(n):
+        l.insert(len(l), input())
+    m.insert(len(m), l)
+
+for i in range(n):
+    for i2 in range(n):
+        if i2 >= st1 and i2 <= st2: 
+            s += m[i3][i2]
+            v += 1
+    st1 += 1
+    st2 -= 1
+    i3 -= 1
+
+if t == "S":
+    print ('{:.1f}').format(s)
+else:
+    print ('{:.1f}').format(s/v)
